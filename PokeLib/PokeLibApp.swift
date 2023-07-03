3929289ad4b3b7d161da708bee4e3f275dce2bce
fajaragungpramana01 <fajar.agungpramana@mareco.id>
2023-07-03 11:06:04 +0700

    Initial commit

---
+++ PokeLib/PokeLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeLibApp.swift
+//  PokeLib
+//
+//  Created by Fajar Agung Pramana on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
