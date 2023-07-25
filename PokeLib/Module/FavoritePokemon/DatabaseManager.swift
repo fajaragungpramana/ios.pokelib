997a074a9ead82aa15e23299c37bc16c3da46f9c
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-07-25 15:31:17 +0700

    Provide local database.

---
+++ PokeLib/Module/FavoritePokemon/DatabaseManager.swift
@@ -0,0 +1,98 @@
+//
+//  DatabaseManager.swift
+//  PokeLib
+//
+//  Created by Fajar Agung Pramana on 25/07/23.
+//
+
+import SQLite
+
+class DatabaseManager {
+    
+    private static var TABLE_NAME = "favorite_pokemons.sqlite3"
+    
+    private var mDb: Connection!
+    private var mFavoritePokemonTable: Table!
+    private var mFavoritePokemonSchema: FavoritePokemonSchema!
+    
+    init() {
+        do {
+            
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            self.mDb = try Connection("\(path)/\(DatabaseManager.TABLE_NAME)")
+            self.mFavoritePokemonTable = Table("pokemons")
+            
+            self.mFavoritePokemonSchema = FavoritePokemonSchema(
+                id: Expression<Double>("id"),
+                name: Expression<String>("name"),
+                image: Expression<String>("image"),
+                about: Expression<String>("about"),
+                height: Expression<Int>("height"),
+                weight: Expression<Int>("weight")
+            )
+            
+            if !CacheManager.shared.isDatabaseManagerCreated {
+                try mDb.run(self.mFavoritePokemonTable.create { t in
+                    t.column(self.mFavoritePokemonSchema.id, primaryKey: true)
+                    t.column(self.mFavoritePokemonSchema.name)
+                    t.column(self.mFavoritePokemonSchema.image)
+                    t.column(self.mFavoritePokemonSchema.about)
+                    t.column(self.mFavoritePokemonSchema.height)
+                    t.column(self.mFavoritePokemonSchema.weight)
+                })
+                
+                CacheManager.shared.isDatabaseManagerCreated = true
+            }
+            
+        } catch {
+            print("DatabaseManager : " + error.localizedDescription)
+        }
+    }
+    
+    func setFavoritePokemon(request: FavoritePokemonRequest) throws -> Bool {
+        do {
+            if try getFavoritePokemon(id: request.id).isEmpty {
+                try self.mDb.run(self.mFavoritePokemonTable.insert(
+                    self.mFavoritePokemonSchema.id <- request.id,
+                    self.mFavoritePokemonSchema.name <- request.name,
+                    self.mFavoritePokemonSchema.image <- request.image,
+                    self.mFavoritePokemonSchema.about <- request.about,
+                    self.mFavoritePokemonSchema.height <- request.height,
+                    self.mFavoritePokemonSchema.weight <- request.weight
+                ))
+                
+                return true
+            }
+            
+            return false
+        } catch {
+            print("DatabaseManager.setFavoritePokemon : " + error.localizedDescription)
+            
+            return false
+        }
+    }
+    
+    func getFavoritePokemon(id: Double) throws -> [FavoritePokemonEntity] {
+        var listFavoritePokemonEntity: [FavoritePokemonEntity] = []
+        let query = self.mFavoritePokemonTable.order(id)
+        do {
+            
+            for pokemon in try self.mDb.prepare(query) {
+                listFavoritePokemonEntity.append(FavoritePokemonEntity(
+                    id: pokemon[self.mFavoritePokemonSchema.id],
+                    name: pokemon[self.mFavoritePokemonSchema.name],
+                    image: pokemon[self.mFavoritePokemonSchema.image],
+                    about: pokemon[self.mFavoritePokemonSchema.about],
+                    height: pokemon[self.mFavoritePokemonSchema.height],
+                    weight: pokemon[self.mFavoritePokemonSchema.weight]
+                ))
+            }
+            
+        } catch {
+            print("DatabaseManager.getFavoritePokemon : " + error.localizedDescription)
+        }
+        
+        return listFavoritePokemonEntity
+    }
+    
+}
